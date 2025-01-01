a0288212c9b3e9602da16ed3acf0f718f652f051
levelhlawrence <levelhlawrence@gmail.com>
2024-12-31 22:57:50 -0500

    created employee and shop models and api views

--- FleetcarePro/server/api/admin.py
+++ FleetcarePro/server/api/admin.py
@@ -1,7 +1,35 @@
 from django.contrib import admin
-from .models import Location, Bus
+from .models import Location, Bus, Employee
 
-# Register your models here.
 
-admin.site.register(Location)
-admin.site.register(Bus)
+# Customize Location admin
+@admin.register(Location)
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ('name', 'address', 'buses_count')
+    readonly_fields = ('get_buses',)
+    search_fields = ('name', 'address')
+    list_filter = ('address',)
+
+    def buses_count(self, obj):
+        return obj.buses.count()
+
+    buses_count.short_description = 'Number of Buses'
+
+    def get_buses(self, obj):
+        return "\n".join(
+            [f"bus: {bus.vehicle} status: {bus.status} vin: {bus.vin} mileage: {bus.current_mileage}" for bus in
+             obj.buses.all()])
+
+    get_buses.short_description = 'Buses at this Location'
+
+
+# Customize Bus admin
+@admin.register(Bus)
+class BusAdmin(admin.ModelAdmin):
+    list_display = ('vehicle', 'vin', 'location', 'status', 'last_service_date')
+    search_fields = ('vin', 'license_plate', 'make', 'model')
+    list_filter = ('status', 'location')
+
+
+# Customize Employee admin
+admin.site.register(Employee)
--- FleetcarePro/server/api/views.py
+++ FleetcarePro/server/api/views.py
@@ -1,9 +1,19 @@
 from rest_framework.viewsets import ModelViewSet
-from .models import Bus
-from .serializers import BusSerializer
+from .models import Bus, Employee, Location
+from .serializers import BusSerializer, LocationSerializer, EmployeeSerializer
 
 
 # Create your views here.
 class BusViewSet(ModelViewSet):
     queryset = Bus.objects.all()
     serializer_class = BusSerializer
+
+
+class LocationViewSet(ModelViewSet):
+    queryset = Location.objects.all()
+    serializer_class = LocationSerializer
+
+
+class EmployeeViewSet(ModelViewSet):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
