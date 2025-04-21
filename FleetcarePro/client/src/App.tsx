93b6cb47b21b522edabc0ef8c9ad9ce36f175a7e
levelhlawrence <levelhlawrence@gmail.com>
2025-04-20 21:05:22 -0400

    half way done with workorder page
    Some route refactoring included

--- FleetcarePro/client/src/App.tsx
+++ FleetcarePro/client/src/App.tsx
@@ -2,10 +2,11 @@ import { Routes, Route } from "react-router-dom";
 import { useLocation } from "react-router-dom";
 import { useState } from "react";
 // importing pages
-import WorkOrder from "@/pages/WorkOrder.tsx";
+import Home from "@/pages/Home";
 import Login from "@/pages/Login.tsx";
 import Settings from "@/pages/Settings.tsx";
 import NavBar from "@/components/NarBar.tsx";
+import WorkOrders from "@/pages/WorkOrders.tsx";
 
 function App() {
   const location = useLocation();
@@ -16,8 +17,14 @@ function App() {
     <div>
       {!hideNavBar && isLoggedIn && <NavBar />}
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="login" element={<Login />} />
-        <Route path="/" element={<WorkOrder />} />
+        <Route path="/workorders" element={<WorkOrders />} />
+        {/* <Route path="/workorders/:id" element={<WorkOrderDetails />} /> */}
+        {/* <Route path="/workorders/create" element={<CreateWorkOrder />} /> */}
+        {/* <Route path="/workorders/edit/:id" element={<EditWorkOrder />} /> */}
+        {/* <Route path="/workorders/delete/:id" element={<DeleteWorkOrder />} /> */}
+        {/* <Route path="/workorders/print/:id" element={<PrintWorkOrder />} /> */}
         <Route path="/settings" element={<Settings />} />
       </Routes>
     </div>
