1f7f6f1ba581b6557ed26659ca2aa99e1e65c167
levelhlawrence <levelhlawrence@gmail.com>
2025-05-04 11:57:42 -0400

    fleet page style and code refactor

--- FleetcarePro/client/src/components/ApiContext.tsx
+++ FleetcarePro/client/src/components/ApiContext.tsx
@@ -5,9 +5,9 @@ const dotenv = import.meta.env;
 const ApiContext = createContext();
 
 export const ApiProvider = ({ children }) => {
+  // ALL STATES
   const [allWorkOrders, setAllWorkOrders] = useState({});
-  const text = "Hello from Context!";
-  const text2 = "Hello from Context number 2!";
+  const [vehicles, setVehicles] = useState(true);
 
   // @GET ALL WORKORDERS
   const getAllWorkOrders = async () => {
@@ -20,12 +20,23 @@ export const ApiProvider = ({ children }) => {
     }
   };
 
-  useEffect(() => {
-    getAllWorkOrders();
-  }, []);
+  // ****VEHICLE APIS BELOW****
+
+  //@GET ALL VEHICLES
+  const getAllVehicles = async (page = 1, pageSize = 10) => {
+    try {
+      const response = await axios.get(
+        `${dotenv.VITE_API_URL}/vehicles?page=${page}&limit=${pageSize}`
+      );
+      setVehicles(response.data);
+      console.log("All vehicles:", response.data);
+    } catch (error) {
+      console.error("Error fetching vehicles:", error);
+    }
+  };
 
   return (
-    <ApiContext.Provider value={{ text, text2, allWorkOrders }}>
+    <ApiContext.Provider value={{ allWorkOrders, getAllVehicles, vehicles }}>
       {children}
     </ApiContext.Provider>
   );
--- FleetcarePro/client/src/components/FleetComponents/DisplayVehicles.tsx
+++ FleetcarePro/client/src/components/FleetComponents/DisplayVehicles.tsx
@@ -1,66 +1,51 @@
 import { useState, useEffect, useRef } from "react";
 import { FaArrowRight } from "react-icons/fa";
 import { FaArrowLeft } from "react-icons/fa";
-import axios from "axios";
-const dotenv = import.meta.env;
+import { IoNewspaperOutline } from "react-icons/io5";
 
-const DisplayVehicles = () => {
-  // show all vehicles by default
-  const [vehicles, setVehicles] = useState(true);
-  const [page, setPage] = useState(1);
-  const [pageSize, setPageSize] = useState(10);
+const DisplayVehicles = ({
+  vehFilterSelect,
+  nextPage,
+  prevPage,
+  page,
+  pageSize,
+  setPageSize,
+  vehicles,
+  getAllVehicles,
+}) => {
   const pageSizeRef = useRef();
-
-  // get all vehicles
-  const getAllVehicles = async () => {
-    try {
-      const response = await axios.get(
-        `${dotenv.VITE_API_URL}/vehicles?page=${page}&limit=${pageSize}`
-      );
-      setVehicles(response.data);
-      console.log("All vehicles:", response.data);
-    } catch (error) {
-      console.error("Error fetching vehicles:", error);
-    }
-  };
 
   // page size selector
   const pageSizeHandeler = () => {
     setPageSize(pageSizeRef.current.value);
   };
 
-  // next page
-  const nextPage = () => {
-    if (page < vehicles?.totalPages) {
-      setPage(page + 1);
-    }
-  };
-  // previous page
-  const prevPage = () => {
-    if (page > 1) {
-      setPage(page - 1);
-    }
-  };
-
   useEffect(() => {
-    getAllVehicles();
+    getAllVehicles(page, pageSize);
   }, [page, pageSize]);
 
   return (
     <div>
       {vehicles && (
         <div>
-          <p className="mb-2">
-            Total Vehicles:{" "}
-            <span className="font-bold">{vehicles?.totalItems}</span>
-          </p>
-
-          <table className="table-auto w-full text-left rounded-md">
+          <div className="flex justify-between mb-2 items-center">
+            <p>
+              Total Vehicles:{" "}
+              <span className="font-bold text-emerald-600">
+                {vehicles?.totalItems}
+              </span>
+            </p>
+            <p className="text-sm">
+              current page: {vehicles?.currentPage} of {vehicles?.totalPages}
+            </p>
+          </div>
+          <table className="table-auto w-full text-left">
             <thead className="text-gray-600">
               <tr className="bg-gray-200 text-sm ">
-                <th className="p-1 rounded-tl">Bus No.</th>
+                <th className="p-1 rounded-tl">Veh No.</th>
                 <th>Make</th>
                 <th>Model</th>
+                {window.innerWidth > 800 && <th>Vin</th>}
                 <th className="rounded-tr">Year</th>
               </tr>
             </thead>
@@ -71,9 +56,13 @@ const DisplayVehicles = () => {
                   className="text-sm hover:bg-emerald-50 hover:cursor-pointer"
                 >
                   <tr className="border-b-2 border-gray-200 hover:border-emerald-400 hover:text-emerald-600">
-                    <td className="py-2">{vehicle.bus_no}</td>
+                    <div className="flex items-center gap-2">
+                      <IoNewspaperOutline />
+                      <td className="py-2">{vehicle.bus_no}</td>
+                    </div>
                     <td>{vehicle.body_make}</td>
                     <td>{vehicle.body_model}</td>
+                    {window.innerWidth > 800 && <td>{vehicle.vin_number}</td>}
                     <td>{parseInt(vehicle.body_year)}</td>
                   </tr>
                 </tbody>
@@ -84,24 +73,40 @@ const DisplayVehicles = () => {
             id="pagination"
             className="flex item-center justify-between mt-4"
           >
-            <p>Items Per Page:</p>
-            <select
-              onChange={pageSizeHandeler}
-              ref={pageSizeRef}
-              name="page-items"
-              id="page-items"
-            >
-              <option value="10">10</option>
-              <option value="20">20</option>
-              <option value="30">30</option>
-              <option value="50">50</option>
-            </select>
-            <p className="mb-4">
-              current page: {vehicles?.currentPage} of {vehicles?.totalPages}
-            </p>
+            <div className="flex items-center text-sm">
+              <p>Items Per Page:</p>
+              <select
+                onChange={pageSizeHandeler}
+                className="border-2 ml-2 w-16 rounded"
+                ref={pageSizeRef}
+                name="page-items"
+                id="page-items"
+              >
+                <option value="10">10</option>
+                <option value="20">20</option>
+                <option value="30">30</option>
+              </select>
+            </div>
+
             <div id="arrow-btns" className="flex gap-4 mr-8">
-              <FaArrowLeft className="cursor-pointer" onClick={prevPage} />
-              <FaArrowRight className="cursor-pointer" onClick={nextPage} />
+              {page > 1 && (
+                <button
+                  className="cursor-pointer flex items-center border rounded gap-4 p-1 hover:bg-gray-200 text-gray-600"
+                  onClick={prevPage}
+                >
+                  <FaArrowLeft />
+                  Prev
+                </button>
+              )}
+              {page !== vehicles.totalPages && (
+                <button
+                  className="cursor-pointer flex items-center border rounded gap-4 p-1 hover:bg-gray-200 text-gray-600"
+                  onClick={nextPage}
+                >
+                  Next
+                  <FaArrowRight />
+                </button>
+              )}
             </div>
           </div>
         </div>
