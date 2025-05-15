dfdef04783192ce5d7e8b2d88f44c483a45ab950
levelhlawrence <levelhlawrence@gmail.com>
2025-05-15 19:31:38 -0400

    commented out user and facility routes

--- FleetcarePro/server/app/routes/userRoutes.ts
+++ FleetcarePro/server/app/routes/userRoutes.ts
@@ -1,27 +1,27 @@
-import express from "express";
-import {
-  getAllUsers,
-  createUser,
-  updateUser,
-  deleteUser,
-  getUserById,
-} from "../controllers/userController";
+// import express from "express";
+// import {
+//   getAllUsers,
+//   createUser,
+//   updateUser,
+//   deleteUser,
+//   getUserById,
+// } from "../controllers/userController";
 
-const userRoutes = express.Router();
+// const userRoutes = express.Router();
 
-// @GET ALL USERS
-userRoutes.get("/", getAllUsers);
+// // @GET ALL USERS
+// userRoutes.get("/", getAllUsers);
 
-// @CREATE USER
-userRoutes.post("/", createUser);
+// // @CREATE USER
+// userRoutes.post("/", createUser);
 
-// @UPDATE USER
-userRoutes.put("/:id", updateUser);
+// // @UPDATE USER
+// userRoutes.put("/:id", updateUser);
 
-// @DELETE USER
-userRoutes.delete("/:id", deleteUser);
+// // @DELETE USER
+// userRoutes.delete("/:id", deleteUser);
 
-// Get user by ID
-userRoutes.get("/:id", getUserById);
+// // Get user by ID
+// userRoutes.get("/:id", getUserById);
 
-export default userRoutes;
+// export default userRoutes;
