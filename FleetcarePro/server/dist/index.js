c302b90a22a632a0f624e1310a86a2d58e7864bd
levelhlawrence <levelhlawrence@gmail.com>
2025-05-15 21:26:51 -0400

    update tsc build

--- FleetcarePro/server/dist/index.js
+++ FleetcarePro/server/dist/index.js
@@ -1,14 +1,8 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const app = (0, express_1.default)();
-const PORT = process.env.PORT || 3000;
-app.get("/", (req, res) => {
-    res.send("<h1>Hello, World!</h1>");
+const server_1 = require("./app/server");
+// Server listening
+server_1.app.listen(server_1.PORT, () => {
+    console.log(`Server is running on http://localhost:${server_1.PORT}`);
 });
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
+//# sourceMappingURL=index.js.map
