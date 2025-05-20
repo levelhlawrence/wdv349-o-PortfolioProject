85a21909b566b7f85a7770d04f3c3698d7add4c3
levelhlawrence <levelhlawrence@gmail.com>
2025-05-20 19:10:55 -0400

    react vechile form working and post request working

--- FleetcarePro/client/src/pages/Home.js
+++
@@ -1,4 +0,0 @@
-import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
-export default function Home() {
-    return (_jsxs("section", { className: "relative md:ml-64", children: [_jsxs("header", { children: [_jsx("h1", { className: "pl-8 py-5 text-2xl font font-bold text-gray-500 border-b-2", children: "Home" }), _jsxs("h2", { className: "pl-8 mt-4 text-gray-500 text-lg", children: [" ", "Welcome, Demo User", " "] })] }), _jsx("div", { className: "flex flex-col md:flex-row justify-between items-center mt-8 mx-8", children: _jsx("p", { children: "This is the Fleet Maintenance Demo. This app is designed to track work orders and fleet details" }) })] }));
-}
--- FleetcarePro/server/app/routes/vehicleRoutes.ts
+++ FleetcarePro/server/app/routes/vehicleRoutes.ts
@@ -2,6 +2,9 @@ import express from "express";
 import {
   getAllVehicles,
   getVehiclesById,
+  createVehicle,
+  updateVehicle,
+  deleteVehicle,
 } from "../controllers/vehicleController";
 
 const vehicleRouter = express.Router();
@@ -11,5 +14,14 @@ vehicleRouter.get("/", getAllVehicles);
 
 // @GET VEHICLE BY ID
 vehicleRouter.get("/:id", getVehiclesById);
+
+// @POST VEHICLE
+vehicleRouter.post("/", createVehicle);
+
+// // @UPDATE VEHICLE
+// vehicleRouter.put("/:id", updateVehicle);
+
+// // @DELETE VEHICLE
+// vehicleRouter.delete("/:id", deleteVehicle);
 
 export { vehicleRouter };
